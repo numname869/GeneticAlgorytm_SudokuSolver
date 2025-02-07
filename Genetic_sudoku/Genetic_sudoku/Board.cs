3e7f545c002ae64b28ff13f3d61d01b62baa85c5
numname869 <izakrzyzowska0@gmail.com>
2025-02-08 00:03:03 +0100

    adding files

---
+++ Genetic_sudoku/Genetic_sudoku/Board.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Genetic_sudoku
+{
+    internal class Board
+    {
+        private int[,] _sudoku;
+
+
+
+        public Board(int[,] sudoku)
+        {
+            _sudoku = sudoku;
+        }
+
+
+        public static void DisplaySudoku(int[,] sudoku)
+        {
+
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+
+                    if (j % 3 == 0 && j != 0)
+                        Console.Write("| ");
+
+                    Console.Write(sudoku[i, j] == 0 ? ". " : sudoku[i, j] + " ");
+
+
+                    if (j == 8)
+                        Console.WriteLine();
+                }
+
+
+                if ((i + 1) % 3 == 0 && i != 8)
+                    Console.WriteLine("------+-------+------");
+            }
+        }
+
+    }
+}
---
+++ Genetic_sudoku/Genetic_sudoku/Generation.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Genetic_sudoku;
+
+Random random = new Random();
+
+namespace Genetic_sudoku
+{
+
+   
+    internal class Generation
+    {
+        //lets say one population is 12 paths
+        //one path is  always lesser than 12 x 81 digits 
+        //path ends when algorythm puts number in incorrect place 
+        //when the generation is done it sorts it by the highest score 
+        // 6 best paths have children - crossover and maybe ? mutation
+
+
+        string _path;
+        string[] _population;
+        
+
+        //will need probably 2 contructors
+        public Generation()
+        {
+            _path = "";
+            _population = new string[10];
+        }
+
+        public void CreatePath(string path)
+        {
+            byte [] digit = new byte [4];
+            random.NextBytes(digit, 0, 1001);
+        }
+
+
+
+    }
+}
